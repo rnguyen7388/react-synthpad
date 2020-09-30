211693092173379410a56e49e11332f473483818
Ray <rnguyen7388@gmail.com>
2020-09-29 20:13:43 -0500

    adds keyboard input

--- src/components/synth/pad.js
+++ src/components/synth/pad.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import useKeyPress from '../../hooks/useKeyPress'
 
 export default ({ letter, note, color, playSound }) => {
-  
+  const isPressed = useKeyPress(letter)
+
   const styles = {
     width: 100,
     height: 100,
     borderRadius: 8,
     background: color,
     cursor: 'pointer',
+    opacity: isPressed ? 1 : .75
   }
 
   return (
-    <button onClick={() => playSound(letter)}style={styles}>{note}</button>
+    <button onClick={() => playSound(letter)} style={styles}>{note}</button>
   )
 }
